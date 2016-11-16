943f8d93d29096da2c601ea5606611d66c28e8fa
kmidas <midas.kamaldeep@gmail.com>
2016-11-15 18:37:21 -0600

    adding non_lambda feature demo class

---
+++ CoreJava/src/org/midas/java8/nonLambda/NewJava8.java
@@ -0,0 +1,12 @@
+package org.midas.java8.nonLambda;
+
+public class NewJava8 {
+	
+	/*
+	 * Feature 1 : There is no PermGen space anymore in java. So no 'OutOfMemory' errors. 
+	 * 
+	 * 
+	 */
+	
+
+}
